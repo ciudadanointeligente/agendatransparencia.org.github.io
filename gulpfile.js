a1cc57bd12ff4c827727d380fb66ab7789f3ad2e
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2015-12-10 10:41:04 -0300

    Adding gulp gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var jekyll = require('gulp-jekyll');
- 
+var ghPages = require('gulp-gh-pages');
+
 gulp.task('default', function () {
     console.log('oli');
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
