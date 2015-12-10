d8e08f8c74d085b92df521417d7db8798764ac32
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2015-12-10 12:02:38 -0300

    Agregandole una funcion de watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var browserSync  = require('browser-sync');
-var cp           = require('child_process');
+var cp = require('child_process');
 
 var messages = {
 	jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
 gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('jekyll', ['build'],
+    	{stdio: 'inherit'}).on('close', done);
+});
+
+gulp.task('jekyll-serve', function (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['serve', '--watch', '--baseurl'],
     	{stdio: 'inherit'}).on('close', done);
@@ -19,4 +25,11 @@ gulp.task('deploy', function() {
     .pipe(ghPages());
 });
 
-gulp.task('default', ['jekyll-build']);
+
+
+
+var watcher = gulp.watch('_cards/**/*.md', function (event) {
+	console.log('El archivo ' + event.path + ' ' + event.type + '!!');
+});
+
+gulp.task('default', ['jekyll-serve']);
