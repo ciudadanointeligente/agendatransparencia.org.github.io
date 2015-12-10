cc13103c3881050be1247438bc6a8385b59193f8
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2015-12-10 11:17:43 -0300

    Agregandole el jekyll serve a gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
-var jekyll = require('gulp-jekyll');
 var ghPages = require('gulp-gh-pages');
+var browserSync  = require('browser-sync');
+var cp           = require('child_process');
 
-gulp.task('default', function () {
-    console.log('oli');
+var messages = {
+	jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('jekyll', ['serve', '--watch', '--baseurl'],
+    	{stdio: 'inherit'}).on('close', done);
 });
+
 
 gulp.task('deploy', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
+
+gulp.task('default', ['jekyll-build']);
